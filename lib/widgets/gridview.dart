e3d3ec6b75ac520f7d009b5e85d980174b31ab1d
刘洋 <liuy@qcubic.com>
2023-08-02 19:47:38 +0800

    fix(登录模块):
    登录发布

--- lib/widgets/gridview.dart
+++ lib/widgets/gridview.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
 
 class GridViewWidget extends StatelessWidget {
-  const GridViewWidget({super.key});
+  GridViewWidget({super.key});
+
+  List<Center> icons = const [
+    Center(child: Icon(Icons.home)),
+    Center(child: Icon(Icons.fastfood_outlined)),
+    Center(child: Icon(Icons.cake_outlined)),
+    Center(child: Icon(Icons.rice_bowl_outlined)),
+    Center(child: Icon(Icons.emoji_food_beverage_outlined)),
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -37,6 +45,33 @@ class GridViewWidget extends StatelessWidget {
               ],
             ),
           ),
+          const Divider(),
+          Expanded(
+            child: GridView.builder(
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 3,
+                  crossAxisSpacing: 10,
+                  mainAxisSpacing: 10,
+                ),
+                itemCount: 6,
+                itemBuilder: (context, index) {
+                  return Container(
+                    height: 400,
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: const Color.fromRGBO(233, 233, 233, 0.9),
+                        width: 1,
+                      ),
+                    ),
+                    child: index < 5
+                        ? icons[index]
+                        : const Text(
+                            '这块采用 builder 方式构建',
+                            textAlign: TextAlign.center,
+                          ),
+                  );
+                }),
+          ),
         ],
       ),
     );
@@ -46,31 +81,72 @@ class GridViewWidget extends StatelessWidget {
 class DynamicGridView extends StatelessWidget {
   const DynamicGridView({super.key});
 
-  List<Widget> _getListData() {
-    List<Widget> listData = [];
-    var tempList = listData.map((value) {
-      return Container(
+  List<Widget> _getDataList(int count) {
+    List<Widget> dataList = [];
+    for (int i = 0; i < count; i++) {
+      Widget widget = Container(
         decoration: BoxDecoration(
-            border: Border.all(
-                color: const Color.fromRGBO(233, 233, 233, 0.9), width: 1)),
+          border: Border.all(
+            color: i % 2 == 0
+                ? const Color.fromRGBO(233, 0, 0, 0.9)
+                : const Color.fromRGBO(0, 0, 233, 0.9),
+            width: 1,
+          ),
+        ),
         child: Column(
           children: <Widget>[
-            Image.network(value['imageUrl']),
             const SizedBox(height: 12),
             Text(
-              value['title'],
+              '这是第 $i 段文本',
               textAlign: TextAlign.center,
               style: const TextStyle(fontSize: 20),
-            )
+            ),
           ],
         ),
       );
-    });
-    return tempList.toList();
+      dataList.add(widget);
+    }
+    return dataList;
+  }
+
+  List<Widget> _getGraphTextList(int count) {
+    List<Widget> dataList = [];
+    for (int i = 0; i < count; i++) {
+      Widget widget = Container(
+        decoration: BoxDecoration(
+          border: Border.all(
+            color: i % 2 == 0
+                ? const Color.fromRGBO(233, 0, 0, 0.9)
+                : const Color.fromRGBO(0, 0, 233, 0.9),
+            width: 1,
+          ),
+        ),
+        child: Column(
+          children: <Widget>[
+            Image.asset('images/$i.png'),
+            const SizedBox(height: 12),
+            Text(
+              '图片 $i',
+              textAlign: TextAlign.center,
+              style: const TextStyle(fontSize: 20),
+            ),
+          ],
+        ),
+      );
+      dataList.add(widget);
+    }
+    return dataList;
   }
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return GridView.count(
+      crossAxisSpacing: 20,
+      mainAxisSpacing: 20,
+      padding: const EdgeInsets.all(10),
+      crossAxisCount: 3,
+      childAspectRatio: 0.8,
+      children: _getDataList(6),
+    );
   }
 }
